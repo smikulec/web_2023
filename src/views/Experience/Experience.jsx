4fdcdaad8aa2b0d79b69fda94e99cbc0cc37e787
Sonja Mikulec <69215212+smikulec@users.noreply.github.com>
2023-04-23 13:26:57 +0000

    Add links to companies

--- src/views/Experience/Experience.jsx
+++ src/views/Experience/Experience.jsx
@@ -1,15 +1,33 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Typography, Link as MUILink } from "@mui/material";
 import { Layout } from "../../components";
 import { forwardRef } from "react";
 
-const SingleSection = ({ role, timeline, company, description }) => {
+const SingleSection = ({
+  role,
+  timeline,
+  company,
+  description,
+  companyUrl,
+}) => {
   return (
     <Box sx={{ pb: 6 }}>
       <Typography variant="h4" sx={{ pb: 1 }}>
         {role}
       </Typography>
       <Typography color="secondary" sx={{ pb: 1 }}>
-        @ {company}
+        @{" "}
+        {companyUrl ? (
+          <MUILink
+            href={companyUrl}
+            target="_blank"
+            underline="hover"
+            color="primary"
+          >
+            {company}
+          </MUILink>
+        ) : (
+          company
+        )}
       </Typography>
       <Typography variant="h6" sx={{ pb: 2 }}>
         {timeline}
@@ -30,6 +48,7 @@ export const Experience = forwardRef((props, ref) => {
             timeline="2021 - present"
             company="Borealis Agency"
             description="I have gained expertise in developing web/desktop applications with NextJS, Typescript, and Electron, testing React components with Jest, collaborating with UX designers, improving features, fixing bugs, implementing mobile-first designs, and writing technical documentation for projects."
+            companyUrl="https://borealis.agency/"
           />
           <SingleSection
             role="Medical doctor"
@@ -41,7 +60,8 @@ export const Experience = forwardRef((props, ref) => {
             role="Medical Specialist & Regulatory Affairs Professional"
             timeline="2016 - 2018"
             company="Pliva / Teva Pharmaceuticals"
-            description="I contributed to writing medical documentation, provided support for the portfolio pipeline, and assisted with medical inquiries from third parties. I also played a key role in reviewing documentation compliance with regulatory requirements throughout the pre- and post-medication registration application process."
+            description="I contributed to writing medical documentation, provided support for the portfolio pipeline, and assisted with inquiries from third parties. I also played a key role in reviewing compliance documentation and regulatory requirements throughout the application process."
+            companyUrl="https://www.tevapharm.com/teva-worldwide/"
           />
         </Layout.Content>
       </Layout>
