c8d8058a178a89c8c2997f373b9abecbcc5d9089
Sonja Mikulec <69215212+smikulec@users.noreply.github.com>
2023-04-23 13:21:46 +0000

    Add changes to props and styling

--- src/components/TextCircle/TextCircle.jsx
+++ src/components/TextCircle/TextCircle.jsx
@@ -1,27 +1,35 @@
-import { Box, Typography, useMediaQuery } from '@mui/material';
+import { Box, Typography, useMediaQuery, Link as MUILink } from "@mui/material";
 
-export const TextCircle = ({ text, icon }) => {
-	const isMobile = useMediaQuery('(max-width: 768px)');
+export const TextCircle = ({ text, icon, textVariant = "h6", url }) => {
+  const isMobile = useMediaQuery("(max-width: 768px)");
 
-	return (
-		<Box
-			sx={{
-				display: 'flex',
-				flexDirection: 'column',
-				justifyContent: 'center',
-				alignItems: 'center',
-			}}>
-			{icon && (
-				<img
-					src={icon}
-					alt={`${text} logo`}
-					width={isMobile ? 40 : 55}
-					height={isMobile ? 40 : 55}
-				/>
-			)}
-			<Typography variant='h6' sx={{ pt: 1.5 }}>
-				{text}
-			</Typography>
-		</Box>
-	);
+  return (
+    <MUILink
+      href={url}
+      target="_blank"
+      underline="none"
+      sx={{ color: "#000000" }}
+    >
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        {icon && (
+          <img
+            src={icon}
+            alt={`${text} logo`}
+            width={isMobile ? 40 : 55}
+            height={isMobile ? 40 : 55}
+          />
+        )}
+        <Typography variant={textVariant} sx={{ pt: 1.5 }}>
+          {text}
+        </Typography>
+      </Box>
+    </MUILink>
+  );
 };
